46dbb3fef53b03e9eea9ade8138bf7df50f0ffe8
ahmedzak2 <48220388+ahmedzak2@users.noreply.github.com>
2021-09-04 22:35:49 +0200

    Add files via upload

---
+++ app/model/User.ts
@@ -0,0 +1,22 @@
+
+export interface adrres {
+  street: string;
+  city: string;
+  state: string;
+  description: string;
+}
+export interface User
+{
+  firstName: string,
+  lastName: string,
+  age: number,
+  address: {
+    street: string,
+    city: string,
+    state: string
+  }
+
+    }
+  
+    
+    
